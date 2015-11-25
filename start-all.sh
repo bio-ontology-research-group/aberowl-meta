5c69c01c58d0be16f1f5fa2c2b883b4c175e292e
Robert Hoehndorf <robert.hoehndorf@kaust.edu.sa>
2015-11-25 22:49:12 +0300

    added start script

---
+++ start-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash                                                                                                                                                                              
+redis-server &
+cd aberowl-server
+export CLASSPATH=.
+for i in lib/*; do export CLASSPATH=$CLASSPATH:$i; done
+groovy AberOWLServer.groovy &
+cd ..
+cd pubmedsearch
+groovy JettyServer.groovy &
+cd ..
+cd rdfsearch
+groovy JettySearchBio2RDF.groovy &
+cd ..
+cd aberowl-web
+PORT=3333 node prerender/server.js&
+sleep 3
+PRERENDER_SERVICE_URL=http://localhost:3333 ./bin/www
